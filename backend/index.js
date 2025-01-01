7b7c227a831eddd93b7741477132beba345c3440
kiranCodings <kiran232377@gmail.com>
2025-01-01 14:47:20 +0530

    fixed deployment error

--- backend/index.js
+++ backend/index.js
@@ -17,13 +17,12 @@ app.use(bodyParser.json());
 connectDB();
 
 
-// Serve the static files from the React app
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-// Handle requests by serving index.html for all routes
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
+
 
 // Routes
 app.use('/api/auth', authRoutes);
